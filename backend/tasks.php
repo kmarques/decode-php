3804a1224f2d3ec638792398ccca5e6ea9cc2058
Karl MARQUES <karl@kmarques.dev>
2025-12-03 11:32:52 +0100

    feat: correction TP Crud

--- backend/tasks.php
+++ backend/tasks.php
@@ -1,6 +1,6 @@
 <?php
 // Model
-$tasks = [["id" => 1, "author" => "dupond", "title" => "dormir", "completed" => true], ["id" => 2, "author" => "durand", "title" => "manger", "completed" => true], ["id" => 3, "author" => "durand", "title" => "manger", "completed" => false], ["id" => 4, "author" => "doe", "title" => "manger", "completed" => false]];
+$tasks = [["id" => "1", "author" => "dupond", "title" => "dormir", "completed" => true], ["id" => "2", "author" => "durand", "title" => "manger", "completed" => true], ["id" => "3", "author" => "durand", "title" => "manger", "completed" => false], ["id" => "4", "author" => "doe", "title" => "manger", "completed" => false]];
 $_GET["completed"] ??= "all";
 $_GET["author"] ??= "";
 
@@ -10,10 +10,42 @@ $isCompleted = $_GET['completed'] === "all" ? null : filter_var($_GET['completed
 $filterOwner = trim($_GET['author']);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $_POST['id'] = uniqid();
-    $tasks[] = $_POST;
-    //ou
-    // array_push($tasks, $_POST);
+    switch ($_POST['action']) {
+        case 'Create':
+            $_POST['id'] = uniqid();
+            $tasks[] = $_POST;
+            //ou
+            // array_push($tasks, $_POST);
+            break;
+        case 'Delete':
+            // $tasks = array_filter($tasks, function ($task) {
+            //     return $task['id'] !== $_POST['id'];
+            // });
+            foreach ($tasks as $index => $task) {
+                if ($task['id'] === $_POST['id']) {
+                    $found = true;
+                    break;
+                }
+            }
+            if (($found ?? false) !== false) {
+                unset($tasks[$index]);
+            }
+            break;
+        case 'Edit':
+            // $tasks = array_map(function ($task) {
+            //     if ($task['id'] === $_POST['id']) {
+            //         return $_POST;
+            //     } else {
+            //         return $task;
+            //     }
+            // }, $tasks);
+            foreach ($tasks as $index => $task) {
+                if ($task['id'] === $_POST['id']) {
+                    $tasks[$index] = $_POST;
+                    break;
+                }
+            }
+    }
 }
 
 $tasksFiltered = array_filter($tasks, function ($task) use ($isCompleted, $filterOwner) {
@@ -82,7 +114,27 @@ $tasksFiltered = array_filter($tasks, function ($task) use ($isCompleted, $filte
         <?php else : ?>
             <ul>
             <?php foreach ($tasksFiltered as $task) : ?>
-                <li style="text-decoration: <?= $task['completed'] ? 'line-through' : 'none' ?>"><?= $task['id'] ?> - <?= $task['title'] ?> - <?= $task['completed'] ? 'completed' : 'not completed' ?> - <?= $task['author'] ?></li>
+                <li style="text-decoration: <?= $task['completed'] ? 'line-through' : 'none' ?>">
+                    <?= $task['id'] ?> - <?= $task['title'] ?> - <?= $task['completed'] ? 'completed' : 'not completed' ?> - <?= $task['author'] ?>
+                    <form method="POST">
+                        <input type="hidden" name="id" value="<?= $task['id'] ?>"/>
+                        <input name="action" type="submit" value="Delete"/>
+                    </form>
+                </li>
+                <li>
+                    <form method="POST">
+                        <input type="hidden" name="id" value="<?= $task['id'] ?>"/>
+                        <span>Titre</span><input name="title" value="<?= $task['title'] ?>"/>
+                        <span>Completed ?</span>
+                        <label>Not Completed <input name="completed" type="radio" value="false" <?=  $task['completed'] ? 'checked' : '' ?> /></label>
+                        <label>Completed <input name="completed" type="radio" value="true" <?=  $task['completed'] ? 'checked' : '' ?> /></label>
+                        <br/>
+                        <span>Author</span>
+                        <input name="author" type="text" value="<?= $task['author'] ?>"/>
+                        <br/>
+                        <input name="action" type="submit" value="Edit"/>
+                    </form>
+                </li>
             <?php endforeach; ?>
             </ul>
         <?php endif; ?>
@@ -97,12 +149,7 @@ $tasksFiltered = array_filter($tasks, function ($task) use ($isCompleted, $filte
             <span>Author</span>
             <input name="author" type="text"/>
             <br/>
-            <input type="submit" value="Create"/>
+            <input name="action" type="submit" value="Create"/>
         </form>
-        <?php if ($_SERVER['REQUEST_METHOD'] === "POST") : ?>
-            <span>Author: <?= $_POST['author'] ?>
-            <span>Title: <?= $_POST['title'] ?>
-            <span>Completed: <?= $_POST['completed'] ?>
-        <?php endif; ?>
 </body>
 </html>
